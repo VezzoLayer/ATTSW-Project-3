3fbbce96786d133826f6c184b9bf84cb409a8efd
VezzoLayer <jacopovezzosiwork@gmail.com>
2025-11-19 16:55:48 +0100

    Creazione classe test per repository, test fallisce conseguente creazione interfaccia repository che estende mongorepository, aggiunta pulizia db prima dei test perche DataMongoTest non sono transactional

---
+++ src/main/java/com/tasks/manager/repositories/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.tasks.manager.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.tasks.manager.model.Task;
+
+public interface TaskRepository extends MongoRepository<Task, String> {
+
+}
--- src/test/java/com/tasks/manager/repositories/TaskRepositoryTest.java
+++ src/test/java/com/tasks/manager/repositories/TaskRepositoryTest.java
@@ -1,22 +1,25 @@
-package com.tasks.manager.mongo;
+package com.tasks.manager.repositories;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collection;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.tasks.manager.model.Task;
 
 @DataMongoTest
 @RunWith(SpringRunner.class)
-public class TaskMongoTest {
+public class TaskRepositoryTest {
+
+	@Autowired
+	private TaskRepository repository;
 
 	@Autowired
 	private MongoTemplate mongoTemplate;
@@ -24,22 +27,14 @@ public class TaskMongoTest {
 	@Before
 	public void setup() {
 		// DataMongoTest non sono transactional, db pulito prima di ogni test
-		mongoTemplate.remove(new Query(), Task.class);
+		repository.deleteAll();
 	}
 
 	@Test
-	public void testTaskMongoMapping() {
+	public void testRepositoryContainsExactlyOneTask() {
 		Task savedTask = mongoTemplate.save(new Task(null, "title", "description", 10, true));
 
-		assertThat(savedTask.getTitle()).isEqualTo("title");
-		assertThat(savedTask.getDescription()).isEqualTo("description");
-		assertThat(savedTask.getPriority()).isEqualTo(10);
-		assertThat(savedTask.isDone()).isTrue();
-
-		assertThat(savedTask.getId()).isNotNull();
-		assertThat(savedTask.getId()).isNotEmpty();
-
-		// Per vedere identifier generato
-		LoggerFactory.getLogger(TaskMongoTest.class).info("Saved: {}", savedTask);
+		Collection<Task> tasks = repository.findAll();
+		assertThat(tasks).containsExactly(savedTask);
 	}
 }
