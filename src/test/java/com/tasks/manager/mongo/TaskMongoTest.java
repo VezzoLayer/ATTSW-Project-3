72cff35fb905b4e473d7653dd26cd0eede454aee
VezzoLayer <jacopovezzosiwork@gmail.com>
2025-11-19 12:04:31 +0100

    Creazione classe test per mapping con mongo, annotazione Document e Id per la classe Task anche se i test passavano comunque grazie alle convenzioni, si esplicitano

---
+++ src/test/java/com/tasks/manager/mongo/TaskMongoTest.java
@@ -0,0 +1,37 @@
+package com.tasks.manager.mongo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.tasks.manager.model.Task;
+
+@DataMongoTest
+@RunWith(SpringRunner.class)
+public class TaskMongoTest {
+
+	@Autowired
+	private MongoTemplate mongoTemplate;
+
+	@Test
+	public void testTaskMongoMapping() {
+		Task savedTask = mongoTemplate.save(new Task(null, "title", "description", 10, true));
+
+		assertThat(savedTask.getTitle()).isEqualTo("title");
+		assertThat(savedTask.getDescription()).isEqualTo("description");
+		assertThat(savedTask.getPriority()).isEqualTo(10);
+		assertThat(savedTask.isDone()).isTrue();
+
+		assertThat(savedTask.getId()).isNotNull();
+		assertThat(savedTask.getId()).isNotEmpty();
+
+		// Per vedere identifier generato
+		LoggerFactory.getLogger(TaskMongoTest.class).info("Saved: {}", savedTask);
+	}
+}
