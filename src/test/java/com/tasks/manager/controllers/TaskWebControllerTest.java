bb48c634db89910abb8781013ecc843fd1b847db
VezzoLayer <jacopovezzosiwork@gmail.com>
2025-11-27 18:17:05 +0100

    Test sul nome della home view

--- src/test/java/com/tasks/manager/controllers/TaskWebControllerTest.java
+++ src/test/java/com/tasks/manager/controllers/TaskWebControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.ModelAndViewAssert;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
@@ -20,5 +21,10 @@ public class TaskWebControllerTest {
 	@Test
 	public void testStatus200() throws Exception {
 		mvc.perform(get("/")).andExpect(status().is2xxSuccessful());
+	}
+
+	@Test
+	public void testReturnHomeView() throws Exception {
+		ModelAndViewAssert.assertViewName(mvc.perform(get("/")).andReturn().getModelAndView(), "index");
 	}
 }
