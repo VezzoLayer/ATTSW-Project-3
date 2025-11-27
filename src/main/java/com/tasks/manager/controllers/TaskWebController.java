b6ecb29d5adaf413395902df89d25296bfefbdb3
VezzoLayer <jacopovezzosiwork@gmail.com>
2025-11-27 18:09:52 +0100

    Creazione classe TaskWebControllerTest e corrispettiva classe TaskWebController, primo test sullo status ed implementazione, creazione file index.html

---
+++ src/main/java/com/tasks/manager/controllers/TaskWebController.java
@@ -0,0 +1,14 @@
+package com.tasks.manager.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TaskWebController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+
+}
