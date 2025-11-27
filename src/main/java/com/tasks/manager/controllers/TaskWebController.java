0b9198dacd1bb65953c5b084eb8b2e5c6350a385
VezzoLayer <jacopovezzosiwork@gmail.com>
2025-11-27 18:52:47 +0100

    Test e implementazione sulla home che fa vedere tasks quando ci sono

--- src/main/java/com/tasks/manager/controllers/TaskWebController.java
+++ src/main/java/com/tasks/manager/controllers/TaskWebController.java
@@ -1,13 +1,26 @@
 package com.tasks.manager.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.tasks.manager.services.TaskService;
 
 @Controller
 public class TaskWebController {
 
+	private TaskService taskService;
+
+	private static final String TASKS_ATTRIBUTE = "tasks";
+
+	public TaskWebController(TaskService taskService) {
+		this.taskService = taskService;
+	}
+
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute(TASKS_ATTRIBUTE, taskService.getAllTasks());
+
 		return "index";
 	}
 
--- src/test/java/com/tasks/manager/controllers/TaskWebControllerTest.java
+++ src/test/java/com/tasks/manager/controllers/TaskWebControllerTest.java
@@ -1,15 +1,25 @@
 package com.tasks.manager.controllers;
 
+import static java.util.Arrays.asList;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.ModelAndViewAssert;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.tasks.manager.model.Task;
+import com.tasks.manager.services.TaskService;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = TaskWebController.class)
@@ -17,6 +27,9 @@ public class TaskWebControllerTest {
 
 	@Autowired
 	private MockMvc mvc;
+
+	@MockitoBean
+	private TaskService taskService;
 
 	@Test
 	public void testStatus200() throws Exception {
@@ -26,5 +39,14 @@ public class TaskWebControllerTest {
 	@Test
 	public void testReturnHomeView() throws Exception {
 		ModelAndViewAssert.assertViewName(mvc.perform(get("/")).andReturn().getModelAndView(), "index");
+	}
+
+	@Test
+	public void testHomeViewShowsTasksWhenThereAreSome() throws Exception {
+		List<Task> tasks = asList(new Task("t1", "test", "test", 10, true));
+
+		when(taskService.getAllTasks()).thenReturn(tasks);
+
+		mvc.perform(get("/")).andExpect(view().name("index")).andExpect(model().attribute("tasks", tasks));
 	}
 }
