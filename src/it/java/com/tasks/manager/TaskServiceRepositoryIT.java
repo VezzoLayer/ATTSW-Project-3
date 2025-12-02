cf7def8ea93d5d37c4f100afdc46878c4c275bf1
VezzoLayer <jacopovezzosiwork@gmail.com>
2025-12-02 18:12:48 +0100

    Creazione class test service repository IT, primo test sul service che può inserire nella repo

---
+++ src/it/java/com/tasks/manager/TaskServiceRepositoryIT.java
@@ -0,0 +1,43 @@
+package com.tasks.manager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.tasks.manager.dto.TaskDTO;
+import com.tasks.manager.model.Task;
+import com.tasks.manager.repositories.TaskRepository;
+import com.tasks.manager.services.TaskService;
+
+@RunWith(SpringRunner.class)
+@DataMongoTest
+@Import(TaskService.class)
+@ActiveProfiles("mongodb")
+public class TaskServiceRepositoryIT {
+
+	@Autowired
+	private TaskService taskService;
+
+	@Autowired
+	private TaskRepository taskRepository;
+
+	@Before
+	public void setup() {
+		// DataMongoTest non sono transactional, db pulito prima di ogni test
+		taskRepository.deleteAll();
+	}
+
+	@Test
+	public void testServiceCanInsertIntoTaskRepository() {
+		Task savedTask = taskService.insertNewTask(new TaskDTO(null, "title", "descr", 10, true));
+
+		assertThat(taskRepository.findById(savedTask.getId())).isPresent();
+	}
+}
